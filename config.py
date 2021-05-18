438c89e79f8b9f56197bc1d35b1458efc9c22d7c
Balaiseee <57873756+Balaiseee@users.noreply.github.com>
2021-05-18 21:04:30 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ, path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+#basedir = path.abspath(path.dirname(__file__))
+#load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
     # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
-    UPLOAD_FOLDER = environ.get('UPLOAD_FOLDER')
+    #UPLOAD_FOLDER = environ.get('UPLOAD_FOLDER')
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
